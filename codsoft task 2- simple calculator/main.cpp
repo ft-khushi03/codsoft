bcb7b6a41f8f04d3509b5ca484f462d0f7e2d996
ft-khushi03 <141167506+ft-khushi03@users.noreply.github.com>
2023-08-02 15:00:33 +0530

    task 2- calculator

---
+++ codsoft task 2- simple calculator/main.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    double num1, num2;
+    char op;
+
+    cout << "Enter the operator (+, -, *, /): ";
+    cin >> op;
+
+    cout << "Enter two numbers one by one: ";
+    cin >> num1 >> num2;
+
+
+    switch (op)
+    {
+
+    case '+':
+        cout << num1 << op << num2 << "=" << num1 + num2 << endl;
+        break;
+
+        case '-':
+        cout << num1 << op << num2<< "=" << num1 - num2 <<endl;
+        break;
+
+        case '*':
+        cout << num1 << op << num2 << "=" << num1 * num2 <<endl;
+        break;
+
+        case '/':
+            if (num2 == 0.0)
+            {
+                cout<< "Divide by Zero Situation" <<endl;
+            }
+            else{
+        cout << num1 << op << num2 <<"=" << num1 / num2 <<endl;
+        break;
+            }
+    default:
+    cout << "Error! operator is not available";
+    break;
+    }
+
+    return 0;
+}
